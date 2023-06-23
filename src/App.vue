bccd0e69435a95d55b78337fee4d086f3802d644
John Roberts <bigjlr2004@yahoo.com>
2023-06-23 09:33:09 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+const greeting = ref('Hello Vue Developer')
+const increaseExcitement = () => {
+  greeting.value += '!'
+  if (greeting.value.endsWith('!!!!')) {
+    imgSrc.value = 'https://media.giphy.com/media/2alKkyRFPKRSU/giphy.gif'
+  }
+}
+const imgSrc = ref('https://media.giphy.com/media/wsXVAJY8ibkqBSmjEF/giphy.gif')
+</script>
+
+<template>
+  <div>
+    <h1>{{ greeting }}</h1>
+    <button @click="increaseExcitement">More Excitement</button>
+    <div v-if="greeting.endsWith('!!!!')">
+      <h2>Ok, we're sufficiently excited!</h2>
+    </div>
+    <div v-else>
+      <p>We're starting to get a little excited...</p>
+    </div>
+    <img :src="imgSrc" :alt="greeting.endsWith('!!!!') ? 'Excitement' : 'Anticipation'" />
+  </div>
+</template>
+
+<style scoped></style>
